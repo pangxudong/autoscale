dd32f31ffe6292b20399eb3b5c0ce064a790b5f1
pangxudong <pangxudong0312@gmail.com>
2017-10-11 16:55:19 +0800

    read write xls, handle row col conversion

--- Rscript-tutorial/test.r
+++ Rscript-tutorial/test.r
@@ -1,6 +1,31 @@
 #!/usr/local/bin/Rscript
 
 # read excel file
-
 library(gdata)
-names<-read.xls("/Users/yuanxu/Downloads/yingling.xls",sheet=1,header=F)
+library(xlsx)
+
+setwd("/Users/yuanxu/Downloads/")
+names<-read.xls("yingling.xls",sheet=1,header=F)
+
+
+# names is data.frame type
+flag = 0
+mdmatrix <- matrix(c("",""), nrow=1)
+for (i in 1:nrow(names)) {
+	for (j in 1:ncol(names[i,])) {
+		if (flag == 0) {
+			temp = names[i,j]
+			flag = 1
+		} else {
+			row = c(as.character(temp),as.character(names[i,j]))
+			mdmatrix <- rbind(mdmatrix, row)
+			flag = 0
+		}
+	}
+}
+
+# convert matrix to dataframe
+mingdan <- data.frame(mdmatrix)
+
+# write to xls
+write.xlsx(mingdan, file="mingdan.xls")
